58307957c7dfeaa823f41c6629d75661be738077
vancanhuit <vancanhuit@gmail.com>
2021-01-03 12:42:49 +0700

    Update

--- src/common/AnsiColor.groovy
+++ src/common/AnsiColor.groovy
@@ -2,7 +2,7 @@ package common
 
 class AnsiColor {
     static final String ANSI_INFO = "\033[01;32m"
-    static final String ANSI_ERROR = "\033[01;35m"
+    static final String ANSI_ERROR = "\033[01;31m"
     static final String ANSI_WARN = "\033[01;33m"
     static final String ANSI_OFF = "\033[00m"
 }
--- vars/log.groovy
+++ vars/log.groovy
@@ -1,13 +1,13 @@
 import static common.AnsiColor.*
 
 void info(String message) {
-    echo "${ANSI_INFO}${message}${ANSI_OFF}"
+    echo "${ANSI_INFO}INFO: ${message}${ANSI_OFF}"
 }
 
 void warn(String message) {
-    echo "${ANSI_WARN}${message}${ANSI_OFF}"
+    echo "${ANSI_WARN}WARNING: ${message}${ANSI_OFF}"
 }
 
 void error(String message) {
-    echo "${ANSI_ERROR}${message}${ANSI_OFF}"
+    echo "${ANSI_ERROR}ERROR: ${message}${ANSI_OFF}"
 }
